886bd27ee57af561d40c42f58128908e6e003a89
Ernesto <totoeas06@gmail.com>
2020-11-18 11:31:46 -0300

    update

---
+++ app/config/env.js
@@ -0,0 +1,16 @@
+const env = {
+    database: 'dcqft2rqkli6v7',
+    username: 'bewyhgyytwxswo',
+    password: '70e3546c97f9ca6624227ac73c399c3de222ff1a8830a31829decd312309af2e',
+    host: 'ec2-100-25-231-126.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+   
+  module.exports = env;
